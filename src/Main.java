48f997ee90c58e95718cc7e4149093e46717027f
hiuchida <hiro4uchida@gmail.com>
2020-10-24 23:16:28 +0900

    template

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 
@@ -7,9 +16,37 @@ public class Main {
 //			int a = sc.nextInt();
 //			int b = sc.nextInt();
 //			int c = sc.nextInt();
+//			int d = sc.nextInt();
+//			int k = sc.nextInt();
+//			int m = sc.nextInt();
+//			long l = sc.nextLong();
+//			int n = sc.nextInt();
 //			String s = sc.nextLine();
-			int ans = 0;
+//			int x = sc.nextInt();
+//			int y = sc.nextInt();
+//			int z = sc.nextInt();
+			int n = 1;
+			int[] ary = new int[n];
+			List<String> list = new ArrayList<>();
+			Deque<Integer> que = new ArrayDeque<>();
+			Set<Integer> set = new HashSet<>();
+			Set<Integer> treeset = new TreeSet<>();
+			long ans = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					ary[i] = 0;
+					list.add("");
+					que.offerFirst(i);
+					que.offerLast(i);
+					set.add(i);
+					treeset.add(i);
+				}
+			}
+			Arrays.sort(ary);
+			Collections.sort(list);
 			System.out.println(ans);
+			System.out.println("Yes");
+			System.out.println("No");
 		}
 	}
 
