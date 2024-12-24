b8069f7c1233f12b1e59cd040d4447c5cae88672
hiuchida <hiro4uchida@gmail.com>
2024-12-24 23:22:50 +0900

    java/abc378/c

---
+++ java/abc378/c/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		List<String> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			Integer x = map.get(a);
+			if (x != null) {
+				list.add(""+x);
+			} else {
+				list.add("-1");
+			}
+			map.put(a, i+1);
+		}
+		System.out.println(String.join(" ", list));
+	}
+}
---
+++ java/abc378/c/README.md
@@ -0,0 +1,3 @@
+# Main
+TreeMap<Integer, Integer> mapに直前の値を保存する。
+結果をList<String> listに貯めこみ、String.joinで1行出力する。
