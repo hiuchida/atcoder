4278cadc906ffe6b2cd5a2d7731b8aabe269867e
hiuchida <hiro4uchida@gmail.com>
2024-12-24 11:35:13 +0900

    fix code style

--- java/abc383/b/Main_hash.java
+++ java/abc383/b/Main_hash.java
@@ -13,12 +13,9 @@ public class Main {
 		}
 		@Override
 		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
+			if (this == obj) return true;
+			if (obj == null) return false;
+			if (getClass() != obj.getClass()) return false;
 			Point other = (Point) obj;
 			return x == other.x && y == other.y;
 		}
