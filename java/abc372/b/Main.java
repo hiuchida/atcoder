8ed90d0506ef118f093b97a7b3dad12c1576a18a
hiuchida <hiro4uchida@gmail.com>
2024-12-29 18:52:30 +0900

    java/abc372/b

---
+++ java/abc372/b/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+		while (m > 0) {
+			for (int i=10; i>=0; i--) {
+				int x = (int)Math.pow(3, i);
+				if (x <= m) {
+					list.add(i);
+					m -= x;
+//					System.out.println(list + " " + m);
+					break;
+				}
+			}
+		}
+		List<String> ls = new ArrayList<>();
+		for (int i : list) {
+			ls.add(""+i);
+		}
+		System.out.println(ls.size());
+		System.out.println(String.join(" ", ls));
+	}
+}
