4ad7bd255d42c5a2bfb0cec6813dfb6df440cac0
hiuchida <hiro4uchida@gmail.com>
2025-01-04 16:28:35 +0900

    java/common/UnionFind_2d

---
+++ java/common/UnionFind_2d.java
@@ -0,0 +1,51 @@
+	static class UnionFind {
+		int h;
+		int w;
+		int[] uf;
+		public UnionFind(int h, int w) {
+			this.h = h;
+			this.w = w;
+			int n = h*w;
+			this.uf = new int[n];
+			for (int i=0; i<n; i++) uf[i] = -1;
+		}
+		public int root(int v) {
+			if (uf[v] < 0) return v;
+			uf[v] = root(uf[v]);
+			return uf[v];
+		}
+		public void merge(int u, int v) {
+			u = root(u);
+			v = root(v);
+			if (u == v) return;
+			int uw = uf[u];
+			int vw = uf[v];
+			if (uw <= vw) {
+				uf[u] += uf[v];
+				uf[v] = u;
+			} else if (uw > vw) {
+				uf[v] += uf[u];
+				uf[u] = v;
+			}
+		}
+		public boolean same(int u, int v) {
+			return root(u) == root(v);
+		}
+		public int size(int v) {
+			v = root(v);
+			return -uf[v];
+		}
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			for (int y=0; y<h; y++) {
+				List<Integer> list = new ArrayList<>();
+				for (int x=0; x<w; x++) {
+					list.add(uf[y*h+x]);
+				}
+				if (sb.length() > 0) sb.append(System.getProperty("line.separator"));
+				sb.append(list);
+			}
+			return sb.toString();
+		}
+	}
--- java/common/staticFields.java
+++ java/common/staticFields.java
@@ -1,5 +1,5 @@
-	static final int[] DY = { -1,1, 0,0 }; //UDLR
-	static final int[] DX = {  0,0,-1,1 }; //UDLR
+	static final int[] DY = { -1, 1, 0, 0 }; //UDLR
+	static final int[] DX = {  0, 0,-1, 1 }; //UDLR
 	static int h; //height=row=y
 	static int w; //width=column=x
 	static int[][] map; //map[h][w]
