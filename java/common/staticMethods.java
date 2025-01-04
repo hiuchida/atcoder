1fc19d0ee0610468bf2d22c192984e398aed5611
hiuchida <hiro4uchida@gmail.com>
2025-01-04 19:58:32 +0900

    java/common/staticMethods gcd

--- java/common/staticMethods.java
+++ java/common/staticMethods.java
@@ -1,11 +1,12 @@
+	static final boolean DEBUG = false;
 	static void DEBUG(Object x) {
-//		System.out.println(x);
+		if (DEBUG) System.out.println(x);
 	}
 	static void DEBUG(long x) {
-		DEBUG(""+x);
+		if (DEBUG) DEBUG(""+x);
     }
 	static void DEBUG(int[] x) {
-		DEBUG(Arrays.toString(x));
+		if (DEBUG) DEBUG(Arrays.toString(x));
     }
 	static void ok() {
 		System.out.println("Yes");
@@ -22,4 +23,12 @@
 			sb.append(ary[i]);
 		}
 		return sb.toString();
+	}
+	static long gcd(long a, long b) {
+		if (a < b) {
+			if (a == 0) return b;
+			else return gcd(a, b % a);
+		}
+		if (b == 0) return a;
+		else return gcd(b, a % b);
 	}
