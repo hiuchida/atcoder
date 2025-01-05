f01518a33915f5d85ce5b4d2dc5f679739866cb8
hiuchida <hiro4uchida@gmail.com>
2025-01-05 12:25:59 +0900

    java/memo/template

---
+++ java/memo/template/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = false;
+	static Scanner sc;
+	long ans = 0;
+	void solve() {
+		int n = nextInt();
+		long l = nextLong();
+		String s = next();
+		int[] ary = nextIntAry(n);
+		DEBUG(ary);
+		System.out.println(ans);
+	}
+
+
+	//---------------------------------------------------------------
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
