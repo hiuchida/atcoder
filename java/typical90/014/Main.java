05b615d2734d35b5fd68d3cfab89e28f69d628c9
hiuchida <hiro4uchida@gmail.com>
2025-01-05 20:14:38 +0900

    java/typical90/014

---
+++ java/typical90/014/Main.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = false;
+	static Scanner sc;
+	long ans = 0;
+	void solve() {
+		int n = nextInt();
+		int[] aa = nextIntAry(n);
+		int[] ab = nextIntAry(n);
+		Arrays.sort(aa);
+		Arrays.sort(ab);
+		DEBUG(aa);
+		DEBUG(ab);
+		for (int i=0; i<n; i++) {
+			ans += Math.abs(aa[i] - ab[i]);
+		}
+		System.out.println(ans);
+	}
+
+
+	//---------------------------------------------------------------
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
