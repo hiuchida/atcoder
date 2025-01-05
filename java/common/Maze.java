788fbc4eb77f4832021fe19d715be5fea7a5445b
hiuchida <hiro4uchida@gmail.com>
2025-01-05 13:07:02 +0900

    java/common/Maze

---
+++ java/common/Maze.java
@@ -0,0 +1,124 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = true;
+	static Scanner sc;
+	long ans = 0;
+	void solve() {
+		int h = nextInt();
+		int w = nextInt();
+		nextLine();
+		Maze mz;
+		mz = new Maze(h, w);
+		mz.initmap();
+		mz.loadmap();
+		mz.printmap();
+	}
+	static class Maze {
+		int h;
+		int w;
+		int[][] map;
+		Maze(int h, int w) {
+			this.h = h;
+			this.w = w;
+			this.map = new int[h + 2][w + 2];
+		}
+		void initmap() {
+			for (int y = 0; y < h + 2; y++) {
+				for (int x = 0; x < w + 2; x++) {
+					map[y][x] = -1;
+				}
+			}
+		}
+		void loadmap() {
+			for (int y = 1; y <= h; y++) {
+				String s = nextLine();
+				for (int x = 1; x <= w; x++) {
+					char ch = s.charAt(x - 1);
+					if (ch == '.') {
+						map[y][x] = 0;
+					} else if (ch == 'S') {
+						map[y][x] = 28;
+					} else if (ch == 'G') {
+						map[y][x] = 16;
+					}
+				}
+			}
+		}
+		void resetmap() {
+			for (int y = 0; y < h + 2; y++) {
+				for (int x = 0; x < w + 2; x++) {
+					if (map[y][x] > 0) map[y][x] = 0;
+				}
+			}
+		}
+		void printmap() {
+			final String tbl = "#.123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+			if (DEBUG) {
+				for (int y = 0; y < h + 2; y++) {
+					for (int x = 0; x < w + 2; x++) {
+						int i=map[y][x]+1;
+						while (i>=tbl.length()) i-=61;
+						if (i<tbl.length()) System.out.print(tbl.charAt(i));
+						else System.out.print('@');
+					}
+					System.out.println();
+				}
+				System.out.println();
+			}
+		}
+	}
+/*
+8 63
+...............................................................
+..S...#............................#####..#####..#####..####G..
+..#...#................................#..#...#......#..#......
+..#####..####...####..####..#..#...#####..#...#..#####..#####..
+..#...#..#..#...#..#..#..#..#..#...#......#...#..#..........#..
+..#...#..#####..####..####..####...#####..#####..#####..#####..
+................#.....#........#...............................
+................#.....#........#...............................
+*/
+
+	//---------------------------------------------------------------
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static String nextLine() {
+		return sc.nextLine();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
