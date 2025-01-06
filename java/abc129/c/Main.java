78978b6232a7c7163533adf6d2fafa3603d810fb
hiuchida <hiro4uchida@gmail.com>
2025-01-06 14:17:37 +0900

    java/abc129/c

---
+++ java/abc129/c/Main.java
@@ -0,0 +1,90 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = false;
+	void solve() {
+		final int M = 1000*1000*1000+7;
+		n = nextInt();
+		int m = nextInt();
+		set = nextIntSet(m);
+		DEBUG(set);
+		long[] dp = new long[n+1];
+		dp[0] = 1;
+		for (int i=0; i<n; i++) {
+			dp[i] %= M;
+			if (!set.contains(i+1)) dp[i+1] += dp[i];
+			if (!set.contains(i+2)) {
+				if (i+1<n) dp[i+2] += dp[i];
+			}
+		}
+		DEBUG(dp);
+		ans = dp[n]%M;
+		System.out.println(ans);
+	}
+	long ans = 0;//Integer.MAX_VALUE;
+	int n;
+	long ln;
+	String s;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Deque<Integer> que = new ArrayDeque<>();
+	TreeSet<Integer> set = new TreeSet<>();
+	TreeMap<Integer,Integer> map = new TreeMap<>();
+	int[] ary;
+/*
+3 1234 abc
+4 7 3
+5 6 2
+*/
+
+	//---------------------------------------------------------------
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static TreeSet<Integer> nextIntSet(int n) {
+		TreeSet<Integer> set = new TreeSet<>();
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			set.add(a);
+		}
+		return set;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void DEBUG(long[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
