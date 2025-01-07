246d2951a653cb03ed956077303219f14a26741f
hiuchida <hiro4uchida@gmail.com>
2025-01-07 09:31:17 +0900

    java/abc098/b

---
+++ java/abc098/b/Main.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = true;
+	void solve() {
+		n = nextInt();
+		s = next();
+		char[] ary = s.toCharArray();
+		for (int i=1; i<n; i++) {
+			set.clear();
+			int c=0;
+			for (int j=0; j<i; j++) {
+				set.add((int)ary[j]);
+			}
+			for (int j=i; j<n; j++) {
+				if (set.contains((int)ary[j])) {
+					set.remove((int)ary[j]);
+					c++;
+				}
+			}
+			ans = Math.max(ans, c);
+		}
+		System.out.println(ans);
+	}
+	long ans = 0;//Integer.MAX_VALUE;
+	int n;
+	long ln;
+	String s;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Deque<Integer> que = new ArrayDeque<>();
+	TreeSet<Integer> set = new TreeSet<>();
+	TreeMap<Integer,Integer> map = new TreeMap<>();
+	int[] ary;
+/*
+3 1234 abc
+4 7 3
+*/
+
+	//---------------------------------------------------------------
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void DEBUG(long[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
