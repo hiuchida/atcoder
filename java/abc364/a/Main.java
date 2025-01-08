8a5a7bd0101fee8e95e54d617b917861f67f8472
hiuchida <hiro4uchida@gmail.com>
2025-01-08 22:13:24 +0900

    java/abc364/a

---
+++ java/abc364/a/Main.java
@@ -0,0 +1,85 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = true;
+	void solve() {
+		n = nextInt();
+		int c = 0;
+		for (int i=0; i<n; i++) {
+			if (c==2) ng();
+			String s = next();
+			if ("sweet".equals(s)) {
+				c++;
+			} else {
+				c=0;
+			}
+		}
+		ok();
+	}
+	long ans = 0;//Integer.MAX_VALUE;
+	int n;
+	long ln;
+	String s;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Deque<Integer> que = new ArrayDeque<>();
+	TreeSet<Integer> set = new TreeSet<>();
+	TreeMap<Integer,Integer> map = new TreeMap<>();
+	int[] ary;
+/*
+3 1234 abc
+4 7 3
+5 6 2
+*/
+
+	//---------------------------------------------------------------
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static TreeSet<Integer> nextIntSet(int n) {
+		TreeSet<Integer> set = new TreeSet<>();
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			set.add(a);
+		}
+		return set;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void DEBUG(long[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
