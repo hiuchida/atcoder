550b02b262d9ecef104da71413527fba46ac0a1c
hiuchida <hiro4uchida@gmail.com>
2025-01-10 01:40:37 +0900

    move to novisteps/05Q/abc378_c

--- java/novisteps/05Q/abc378_c/Main.java
+++ java/novisteps/05Q/abc378_c/Main.java
@@ -8,13 +8,17 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			int a = sc.nextInt();
 			Integer x = map.get(a);
-			if (x != null) {
-				list.add(""+x);
-			} else {
-				list.add("-1");
-			}
+			if (x != null) list.add(""+x);
+			else list.add("-1");
 			map.put(a, i+1);
 		}
 		System.out.println(String.join(" ", list));
 	}
 }
+/*
+5
+1 2 1 1 3
+
+4
+1 1000000000 1000000000 1
+*/
