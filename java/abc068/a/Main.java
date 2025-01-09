d84e24fb111893d354e064f65fc4837b0ea7c739
hiuchida <hiro4uchida@gmail.com>
2025-01-09 15:25:22 +0900

    java/abc068/a

---
+++ java/abc068/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String s = "ABC" + n;
+		System.out.println(s);
+	}
+}
+/*
+100
+
+425
+
+999
+*/
