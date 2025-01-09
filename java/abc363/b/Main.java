78b5030a35fb412097755840ad7436a6887e3e2e
hiuchida <hiro4uchida@gmail.com>
2025-01-09 18:36:55 +0900

    java/abc363/b

---
+++ java/abc363/b/Main.java
@@ -0,0 +1,96 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = false;
+	void solve() {
+		n = nextInt();
+		int t = nextInt();
+		int p = nextInt();
+		ary = nextIntAry(n);
+		Arrays.sort(ary);
+		DEBUG(ary);
+		int c=0;
+		for (int i=n-1; i>=0; i--) {
+			DEBUG(t + " " + p + " " + c);
+			if (ary[i] >= t) p--;
+			else {
+				c+=t-ary[i];
+				t=ary[i];
+				p--;
+			}
+			if (p == 0) break;
+		}
+		System.out.println(c);
+	}
+	long ans = 0;//Integer.MAX_VALUE;
+	int n;
+	long ln;
+	String s;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Deque<Integer> que = new ArrayDeque<>();
+	TreeSet<Integer> set = new TreeSet<>();
+	TreeMap<Integer,Integer> map = new TreeMap<>();
+	int[] ary;
+/*
+5 10 3
+3 11 1 6 2
+
+2 5 2
+10 10
+
+3 10 1
+1 2 3
+*/
+
+	//---------------------------------------------------------------
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static TreeSet<Integer> nextIntSet(int n) {
+		TreeSet<Integer> set = new TreeSet<>();
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			set.add(a);
+		}
+		return set;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void DEBUG(long[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
