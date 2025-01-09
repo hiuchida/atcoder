6fea22c4a07bb2160c9c7c518d830f3a33870358
hiuchida <hiro4uchida@gmail.com>
2025-01-09 14:52:58 +0900

    java/abs/a

---
+++ java/abs/a/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		String s = sc.next();
+		int sum = a + b + c;
+		System.out.println(sum + " " + s);
+	}
+}
+/*
+1
+2 3
+test
+*/
