9d1687bc504521c0df2b4f3bdcdf9889c673db9b
hiuchida <hiro4uchida@gmail.com>
2025-01-09 19:15:12 +0900

    move to novisteps/09Q/abc048_a

---
+++ java/novisteps/09Q/abc048_a/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println("A" + s.charAt(8) + "C");
+	}
+}
+/*
+AtCoder Beginner Contest
+
+AtCoder Snuke Contest
+
+AtCoder X Contest
+*/
