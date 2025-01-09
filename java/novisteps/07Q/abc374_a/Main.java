9bd10dde2ffad118a49431fc15ac3a6d61851573
hiuchida <hiro4uchida@gmail.com>
2025-01-09 21:57:35 +0900

    move to novisteps/07Q/abc374_a

--- java/novisteps/07Q/abc374_a/Main.java
+++ java/novisteps/07Q/abc374_a/Main.java
@@ -3,9 +3,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		if (s.endsWith("san"))
-			System.out.println("Yes");
-		else
-			System.out.println("No");
+		if (s.endsWith("san")) System.out.println("Yes");
+		else System.out.println("No");
 	}
 }
+/*
+takahashisan
+
+aokikun
+*/
