ae27a2a80b644f78c45258336b71fbdfddaee1f0
hiuchida <hiro4uchida@gmail.com>
2025-01-09 20:09:31 +0900

    move to novisteps/08Q/abc051_a

---
+++ java/novisteps/08Q/abc051_a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		s=s.replaceAll(","," ");
+		System.out.println(s);
+	}
+}
+/*
+happy,newyear,enjoy
+
+haiku,atcoder,tasks
+
+abcde,fghihgf,edcba
+*/
