c0ab010e3dc8b7d03839e93308f44a5de48724be
hiuchida <hiro4uchida@gmail.com>
2025-01-09 22:42:20 +0900

    move to novisteps/06Q/abc386_b

---
+++ java/novisteps/06Q/abc386_b/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		long ans = 0;
+		int z = 0;
+		for (int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == '0') {
+				z++;
+			} else {
+				ans += z/2;
+				ans += z%2;
+				z = 0;
+				ans++;
+			}
+		}
+		if (z > 0) {
+			ans += z/2;
+			ans += z%2;
+		}
+		System.out.println(ans);
+	}
+}
+/*
+1000000007
+
+998244353
+
+32000
+*/
