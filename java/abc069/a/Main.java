777f436f65ea592f8b64664fd22f47f3cf6dc45e
hiuchida <hiro4uchida@gmail.com>
2025-01-09 15:19:38 +0900

    java/abc069/a

---
+++ java/abc069/a/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		n--;
+		m--;
+		int sum = n * m;
+		System.out.println(sum);
+	}
+}
+/*
+3 4
+
+2 2
+*/
