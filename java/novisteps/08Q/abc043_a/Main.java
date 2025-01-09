ae21774c5c9ee9ef56d67079b2f07ba786bece97
hiuchida <hiro4uchida@gmail.com>
2025-01-09 20:20:35 +0900

    move to novisteps/08Q/abc043_a

---
+++ java/novisteps/08Q/abc043_a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long sum = n*(n+1)/2;
+		System.out.println(sum);
+	}
+}
+/*
+3
+
+10
+
+1
+*/
