1d984d70b029b4fe0cde259006b579df78231c8d
hiuchida <hiro4uchida@gmail.com>
2025-01-09 19:18:48 +0900

    move to novisteps/09Q/abc045_a

---
+++ java/novisteps/09Q/abc045_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int h = sc.nextInt();
+		int ans = (a+b)*h/2;
+		System.out.println(ans);
+	}
+}
+/*
+3
+4
+2
+
+4
+4
+4
+*/
