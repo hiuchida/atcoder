79baf6385645367c57b5368efd2186d95f81087c
hiuchida <hiro4uchida@gmail.com>
2025-01-11 08:59:35 +0900

    move to novisteps/09Q/abc099_a

---
+++ java/novisteps/09Q/abc099_a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n<1000) System.out.println("ABC");
+		else System.out.println("ABD");
+	}
+}
+/*
+999
+
+1000
+
+1481
+*/
