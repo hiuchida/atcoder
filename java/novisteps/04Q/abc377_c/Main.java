03d0f1baa6b60bf4ecdc8d1486d7614fbc6a8932
hiuchida <hiro4uchida@gmail.com>
2025-01-10 15:32:36 +0900

    move to novisteps/04Q/abc377_c

---
+++ java/novisteps/04Q/abc377_c/Main.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class Main {
+	static int n;
+	static Set<Point> set = new TreeSet<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] dy = { 2, 1,-1,-2,-2,-1, 1, 2 };
+		int[] dx = { 1, 2, 2, 1,-1,-2,-2,-1 };
+		for (int i = 0; i < m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			add(a, b);
+			for (int d=0; d<dy.length; d++) {
+				add(a+dy[d], b+dx[d]);
+			}
+		}
+		long ans = (long)n * n - set.size();
+		System.out.println(ans);
+	}
+	static void add(int y, int x) {
+		if (y < 1 || y > n || x < 1 || x > n) return;
+		set.add(new Point(y, x));
+	}
+
+	static class Point implements Comparable<Point> {
+		int y;
+		int x;
+		Point(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+		@Override
+		public int compareTo(Main.Point that) {
+			int cmp = Integer.compare(this.y, that.y);
+			if (cmp != 0) return cmp;
+			return Integer.compare(this.x, that.x);
+		}
+		@Override
+		public String toString() {
+			return "(" + y + "," + x + ")";
+		}
+	}
+}
+/*
+8 6
+1 4
+2 1
+3 8
+4 5
+5 2
+8 3
+
+1000000000 1
+1 1
+
+20 10
+1 4
+7 11
+7 15
+8 10
+11 6
+12 5
+13 1
+15 2
+20 10
+20 15
+*/
