06fe18a3da39841465a7a5b53bb9ddd455a48ead
hiuchida <hiro4uchida@gmail.com>
2025-01-11 01:20:49 +0900

    java/abc091/a

---
+++ java/abc091/a/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if (a+b>=c) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
+/*
+50 100 120
+
+500 100 1000
+
+19 123 143
+
+19 123 142
+*/
