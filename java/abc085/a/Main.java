795666fd0ffffe4f96678e9de3410c14c330630a
hiuchida <hiro4uchida@gmail.com>
2025-01-11 01:02:13 +0900

    java/abc085/a

---
+++ java/abc085/a/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s=sc.next();
+		String ans=s.replaceAll("2017", "2018");
+		System.out.println(ans);
+	}
+}
+/*
+2017/01/07
+
+2017/01/31
+*/
