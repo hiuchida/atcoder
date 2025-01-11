feb550f810722783c89e5fca4f778ccadbb8f545
hiuchida <hiro4uchida@gmail.com>
2025-01-11 09:28:49 +0900

    java/abc075/a

---
+++ java/abc075/a/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] ary = new int[3];
+		for (int i=0; i<ary.length; i++) {
+			int a = sc.nextInt();
+			ary[i] = a;
+		}
+		Arrays.sort(ary);
+		if (ary[0]==ary[1]) System.out.println(ary[2]);
+		else System.out.println(ary[0]);
+	}
+}
+/*
+5 7 5
+
+1 1 7
+
+-100 100 100
+*/
