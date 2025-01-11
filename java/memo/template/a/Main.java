b41158ec535f7073d94d2c7a1716b2cf4e9ce320
hiuchida <hiro4uchida@gmail.com>
2025-01-11 19:45:48 +0900

    java/memo/template/a

--- java/memo/template/a/Main.java
+++ java/memo/template/a/Main.java
@@ -6,17 +6,30 @@ public class Main {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		String s = sc.next();
+		int n = sc.nextInt();
+		int[] ary = new int[n];
+		int ans = 0;
+		for (int i=0; i<n; i++) {
+			ary[i] = sc.nextInt();
+			ans += ary[i];
+		}
 		int sum = a + b + c;
 		if (sum < 10) System.out.println(sum + " " + s);
 		else System.out.println("error");
+		System.out.println(ans);
+		System.out.println("Yes");
+		System.out.println("No");
+		System.exit(0);
 	}
 }
 /*
-1
-2 3
+1 2 3
 test
-
 3
-4 5
+3 2 1
+
+3 4 5
 test2
+3
+5 4 3
 */
