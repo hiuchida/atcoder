58ddf95d7c2b2109310abbab85c1e634ffb85d3a
hiuchida <hiro4uchida@gmail.com>
2025-01-11 23:08:06 +0900

    java/abc090/a

---
+++ java/abc0xx/abc090/a/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i=0; i<3; i++) {
+			String s = sc.next();
+			System.out.print(s.charAt(i));
+		}
+		System.out.println();
+	}
+}
+/*
+ant
+obe
+rec
+
+edu
+cat
+ion
+*/
