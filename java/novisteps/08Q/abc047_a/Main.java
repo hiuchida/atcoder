80256b18be8aa80c1c8d169861aa0d77e5c27a1c
hiuchida <hiro4uchida@gmail.com>
2025-01-11 19:45:04 +0900

    fix java/novisteps/08Q/abc047_a

--- java/novisteps/08Q/abc047_a/Main.java
+++ java/novisteps/08Q/abc047_a/Main.java
@@ -2,11 +2,10 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i=0;
 		int[] ary = new int[3];
-		ary[i++] = sc.nextInt();
-		ary[i++] = sc.nextInt();
-		ary[i++] = sc.nextInt();
+		for (int i=0; i<ary.length; i++) {
+			ary[i] = sc.nextInt();
+		}
 		Arrays.sort(ary);
 		if (ary[0]+ary[1] == ary[2]) System.out.println("Yes");
 		else System.out.println("No");
