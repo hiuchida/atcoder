60dbefcf15ca70c5ee6e9e450002a465d05a32af
hiuchida <hiro4uchida@gmail.com>
2025-01-11 19:51:32 +0900

    fix java/novisteps/08Q/abc066_a

--- java/novisteps/08Q/abc066_a/Main.java
+++ java/novisteps/08Q/abc066_a/Main.java
@@ -3,9 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] ary = new int[3];
-		for (int i=0; i<3; i++) {
-			int a = sc.nextInt();
-			ary[i] = a;
+		for (int i=0; i<ary.length; i++) {
+			ary[i] = sc.nextInt();
 		}
 		Arrays.sort(ary);
 		int ans=ary[0]+ary[1];
