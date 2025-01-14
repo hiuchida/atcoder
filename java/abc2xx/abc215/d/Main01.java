e33b12ee9ecfde3cbb418f30b028982451c927d9
hiuchida <hiro4uchida@gmail.com>
2025-01-14 20:30:06 +0900

    java/abc215/d

---
+++ java/abc2xx/abc215/d/Main01.java
@@ -0,0 +1,114 @@
+import java.util.*;
+public class Main {
+	static final boolean DEBUG = false;
+	void solve() {
+		n = nextInt();
+		int m = nextInt();
+		ary = nextIntAry(n);
+		Arrays.sort(ary);
+		DEBUG(ary);
+		Prime pr = new Prime(m);
+		set.addAll(pr.list);
+		DEBUG(set);
+		for (int i=0; i<n; i++) {
+			int x=ary[i];
+			for (int p:pr.list) {
+				if (x<p) break;
+				if (x%p==0) set.remove(p);
+			}
+		}
+		set.add(1);
+		System.out.println(set.size());
+		for (int i:set) {
+			System.out.println(i);
+		}
+	}
+	long ans = 0;//Integer.MAX_VALUE;
+	int n;
+	long ln;
+	String s;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Deque<Integer> que = new ArrayDeque<>();
+	TreeSet<Integer> set = new TreeSet<>();
+	TreeMap<Integer,Integer> map = new TreeMap<>();
+	int[] ary;
+/*
+3 1234 abc
+4 7 3
+5 6 2
+*/
+
+	static class Prime {
+		int n;
+		boolean[] isp;
+		List<Integer> list = new ArrayList<>();
+		Prime(int n) {
+			this.n = n;
+			this.isp = new boolean[n];
+			init();
+		}
+		void init() {
+			isp[0] = false;
+			isp[1] = false;
+			for (int i=2; i<n; i++) isp[i] = true;
+			for (int i=2; i<n; i++) {
+				if (isp[i]) {
+					list.add(i);
+					for (int j=i*2; j<n; j+=i) isp[j] = false;
+				}
+			}
+		}
+	}
+	//---------------------------------------------------------------
+	static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		new Main().solve();
+	}
+	static int nextInt() {
+		return sc.nextInt();
+	}
+	static long nextLong() {
+		return sc.nextLong();
+	}
+	static String next() {
+		return sc.next();
+	}
+	static int[] nextIntAry(int n) {
+		int[] ary = new int[n];
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			ary[i] = a;
+		}
+		return ary;
+	}
+	static TreeSet<Integer> nextIntSet(int n) {
+		TreeSet<Integer> set = new TreeSet<>();
+		for (int i=0; i<n; i++) {
+			int a = nextInt();
+			set.add(a);
+		}
+		return set;
+	}
+	static void DEBUG(Object x) {
+		if (DEBUG) System.out.println(x);
+	}
+	static void DEBUG(long x) {
+		if (DEBUG) DEBUG(""+x);
+    }
+	static void DEBUG(int[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void DEBUG(long[] x) {
+		if (DEBUG) DEBUG(Arrays.toString(x));
+    }
+	static void ok() {
+		System.out.println("Yes");
+		System.exit(0);
+	}
+	static void ng() {
+		System.out.println("No");
+		System.exit(0);
+	}
+}
