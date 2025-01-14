bc196b6e5d18b3220c3beca6dbd936147e281118
hiuchida <hiro4uchida@gmail.com>
2025-01-14 20:18:02 +0900

    java/abc215/a

---
+++ java/abc2xx/abc215/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if ("Hello,World!".equals(s)) System.out.println("AC");
+		else System.out.println("WA");
+	}
+}
+/*
+Hello,World!
+
+Hello,world!
+
+Hello!World!
+*/
