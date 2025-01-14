d3e357f23f24d75e891f0898ffaa775f95874745
hiuchida <hiro4uchida@gmail.com>
2025-01-14 19:39:45 +0900

    java/abc057/b

---
+++ java/abc0xx/abc057/b/Main.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] anx = new int[n];
+		int[] any = new int[n];
+		for (int i=0; i<n; i++) {
+			anx[i] = sc.nextInt();
+			any[i] = sc.nextInt();
+		}
+		int[] amx = new int[m];
+		int[] amy = new int[m];
+		for (int i=0; i<m; i++) {
+			amx[i] = sc.nextInt();
+			amy[i] = sc.nextInt();
+		}
+		for (int i=0; i<n; i++) {
+			int ans = 0;
+			int min = Integer.MAX_VALUE;
+			for (int j=0; j<m; j++) {
+				int d=calc(anx[i], any[i], amx[j], amy[j]);
+				if (min>d) {
+					min=d;
+					ans=j+1;
+				}
+			}
+			System.out.println(ans);
+		}
+	}
+	static int calc(int x1, int y1, int x2, int y2) {
+		return Math.abs(x1-x2)+Math.abs(y1-y2);
+	}
+}
+/*
+2 2
+2 0
+0 0
+-1 0
+1 0
+
+3 4
+10 10
+-10 -10
+3 3
+1 2
+2 3
+3 5
+3 5
+
+5 5
+-100000000 -100000000
+-100000000 100000000
+100000000 -100000000
+100000000 100000000
+0 0
+0 0
+100000000 100000000
+100000000 -100000000
+-100000000 100000000
+-100000000 -100000000
+*/
