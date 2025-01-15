5dc8575f0aa216d3e001383ffd742809d7d2ea12
hiuchida <hiro4uchida@gmail.com>
2025-01-15 22:27:09 +0900

    java/abc217/a

---
+++ java/abc2xx/abc217/a/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String t = sc.next();
+		if (s.compareTo(t)<0) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
+/*
+abc atcoder
+
+arc agc
+
+a aa
+*/
