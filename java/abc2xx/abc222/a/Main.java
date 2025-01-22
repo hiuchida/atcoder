c2d8888e8a88f33af94432793f1c71e249162f6d
hiuchida <hiro4uchida@gmail.com>
2025-01-22 17:53:15 +0900

    java/abc222/a

---
+++ java/abc2xx/abc222/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		while (n.length()<4) n="0"+n;
+		System.out.println(n);
+	}
+}
+/*
+321
+
+7777
+
+1
+*/
