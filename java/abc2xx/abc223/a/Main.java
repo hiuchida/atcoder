65eab1d525eeeca5cda78484004305ae4a237923
hiuchida <hiro4uchida@gmail.com>
2025-01-23 17:15:39 +0900

    java/abc223/a

---
+++ java/abc2xx/abc223/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x>0 && x%100==0) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
+/*
+500
+
+40
+
+0
+*/
