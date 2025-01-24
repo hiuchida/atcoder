bbaa4a9022c3aabaa21c64a3b96e78b216395035
hiuchida <hiro4uchida@gmail.com>
2025-01-24 09:32:16 +0900

    java/abc224/a

---
+++ java/abc2xx/abc224/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if (s.endsWith("er")) System.out.println("er");
+		else System.out.println("ist");
+	}
+}
+/*
+atcoder
+
+tourist
+
+er
+*/
