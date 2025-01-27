356bbd94b10a1ddfb1e8e56752a11e8cd9206f7b
hiuchida <hiro4uchida@gmail.com>
2025-01-28 08:36:50 +0900

    move to novisteps/06Q/abc228_a

---
+++ java/novisteps/06Q/abc228_a/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		int t = sc.nextInt();
+		int x = sc.nextInt();
+		while (s != t) {
+			if (s<=x && x<s+1) {
+				System.out.println("Yes");
+				System.exit(0);
+			}
+			s++;
+			s%=24;
+		}
+		System.out.println("No");
+	}
+}
+/*
+7 20 12
+
+20 7 12
+
+23 0 23
+*/
