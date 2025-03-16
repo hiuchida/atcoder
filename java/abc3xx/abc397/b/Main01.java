810a31df84fe944056a82a5581fd02cefa477986
hiuchida <hiro4uchida@gmail.com>
2025-03-16 15:51:38 +0900

    java/abc397/b

---
+++ java/abc3xx/abc397/b/Main01.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int ans=0;
+		int idx=1;
+		for (int i=0; i<s.length(); i++) {
+			char ch=s.charAt(i);
+			if (idx%2==1) {
+				if (ch=='o') {
+					ans++;
+				}
+			} else {
+				if (ch=='i') {
+					ans++;
+				}
+			}
+			idx++;
+		}
+		if (s.charAt(s.length()-1)=='i') ans++;
+		System.out.println(ans);
+	}
+}
+/*
+ioi
+
+iioo
+
+io
+*/
