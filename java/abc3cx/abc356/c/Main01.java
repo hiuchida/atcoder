be141ffc0259f955a6f908b8b773cde50ab36eac
hiuchida <hiro4uchida@gmail.com>
2025-03-16 18:19:42 +0900

    java/abc356/c

---
+++ java/abc3cx/abc356/c/Main01.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+		for (int i=0; i<m; i++) {
+			int c = sc.nextInt();
+			int a = sc.nextInt();
+			String r = sc.next();
+		}
+		System.out.println();
+		System.exit(0);
+	}
+}
+/*
+3 2 2
+3 1 2 3 o
+2 2 3 x
+
+4 5 3
+3 1 2 3 o
+3 2 3 4 o
+3 3 4 1 o
+3 4 1 2 o
+4 1 2 3 4 x
+
+11 4 9
+10 1 2 3 4 5 6 7 8 9 10 o
+11 1 2 3 4 5 6 7 8 9 10 11 o
+10 11 10 9 8 7 6 5 4 3 2 x
+10 11 9 1 4 3 7 5 6 2 10 x
+*/
