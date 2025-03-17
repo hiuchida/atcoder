53029f2286b7d972d26a391a619fe9c87239f37b
hiuchida <hiro4uchida@gmail.com>
2025-03-17 15:27:33 +0900

    java/abc349/b

---
+++ java/abc3bx/abc349/b/Main01.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int[] ary=new int[26];
+		for (int i=0; i<s.length(); i++) {
+			ary[s.charAt(i)-'a']++;
+		}
+		int[] cnt=new int[100];
+		for (int i=0; i<ary.length; i++) {
+			cnt[ary[i]]++;
+		}
+//		System.out.println(Arrays.toString(cnt));
+		for (int i=1; i<cnt.length; i++) {
+			if (cnt[i]!=0 && cnt[i]!=2) {
+				System.out.println("No");
+				System.exit(0);
+			}
+		}
+		System.out.println("Yes");
+	}
+}
+/*
+commencement
+
+banana
+
+ab
+*/
---
+++ java/abc3bx/abc349/b/README.md
@@ -0,0 +1,12 @@
+# Main01
+int[] aryに文字コードごとのカウントを取る。
+int[] cntにカウントのカウントを取る。
+iを1からcnt.size-1まで、cnt\[i\]が0または2をチェックする。
+RE1個。
+
+# Main
+int[] aryに文字コードごとのカウントを取る。
+int[] cntにカウントのカウントを取る。
+iを1からcnt.size-1まで、cnt\[i\]が0または2をチェックする。
+
+int[] cnt=new int[100];をint[] cnt=new int[101];に修正する。
