6bc14faa6e49ee28a6f767f9b903432939c42a31
hiuchida <hiro4uchida@gmail.com>
2025-03-17 19:49:08 +0900

    java/abc354/c

---
+++ java/abc3cx/abc354/c/Main.java
@@ -0,0 +1,146 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		TreeSet<Point> set1=new TreeSet<>();
+		TreeSet<Point2> set2=new TreeSet<>();
+		for (int i=0; i<n; i++) {
+			int a = sc.nextInt();
+			int c = sc.nextInt();
+			set1.add(new Point(a, c, i+1));
+			set2.add(new Point2(a, c, i+1));
+		}
+//		System.out.println(set1);
+//		System.out.println(set2);
+		TreeSet<Point3> set3=new TreeSet<>();
+		while (set1.size()>0) {
+			Point p1=set1.last();
+			set1.remove(p1);
+			set2.remove(new Point2(p1.st, p1.ed, p1.idx));
+			set3.add(new Point3(p1.st, p1.ed, p1.idx));
+			while (set2.size()>0) {
+				Point2 p2=set2.last();
+//				System.out.println(p1+" "+p2);
+				if (p2.ed<p1.ed) break;
+				set2.remove(p2);
+				set1.remove(new Point(p2.st, p2.ed, p2.idx));
+			}
+		}
+//		System.out.println(set1);
+//		System.out.println(set2);
+//		System.out.println(set3);
+		System.out.println(set3.size());
+		for (Point3 p3 : set3) {
+			System.out.print(p3.idx+" ");
+		}
+		System.out.println();
+	}
+	static class Point implements Comparable<Point> {
+		final int st;
+		final int ed;
+		final int idx;
+		Point(int st, int ed, int idx) {
+			this.st = st;
+			this.ed = ed;
+			this.idx = idx;
+		}
+		@Override
+		public int compareTo(Main.Point that) {
+			int cmp = Integer.compare(this.st, that.st);
+			if (cmp != 0) return cmp;
+			return Integer.compare(this.ed, that.ed);
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(st, ed);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			Point that = (Point) obj;
+			return this.st == that.st && this.ed == that.ed;
+		}
+		@Override
+		public String toString() {
+			return "(" + st + "," + ed + "," + idx + ")";
+		}
+	}
+	static class Point2 implements Comparable<Point2> {
+		final int st;
+		final int ed;
+		final int idx;
+		Point2(int st, int ed, int idx) {
+			this.st = st;
+			this.ed = ed;
+			this.idx = idx;
+		}
+		@Override
+		public int compareTo(Main.Point2 that) {
+			int cmp = Integer.compare(this.ed, that.ed);
+			if (cmp != 0) return cmp;
+			return Integer.compare(this.st, that.st);
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(st, ed);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			Point that = (Point) obj;
+			return this.st == that.st && this.ed == that.ed;
+		}
+		@Override
+		public String toString() {
+			return "(" + st + "," + ed + "," + idx + ")";
+		}
+	}
+	static class Point3 implements Comparable<Point3> {
+		final int st;
+		final int ed;
+		final int idx;
+		Point3(int st, int ed, int idx) {
+			this.st = st;
+			this.ed = ed;
+			this.idx = idx;
+		}
+		@Override
+		public int compareTo(Main.Point3 that) {
+			int cmp = Integer.compare(this.idx, that.idx);
+			return cmp;
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(st, ed);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			Point that = (Point) obj;
+			return this.st == that.st && this.ed == that.ed;
+		}
+		@Override
+		public String toString() {
+			return "(" + st + "," + ed + "," + idx + ")";
+		}
+	}
+}
+/*
+3
+2 4
+1 1
+3 2
+
+5
+1 1
+10 2
+100 3
+1000 4
+10000 5
+
+6
+32 101
+65 78
+2 29
+46 55
+103 130
+52 40
+*/
---
+++ java/abc3cx/abc354/c/Main_fix.java
@@ -0,0 +1,102 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		TreeSet<Point> set1=new TreeSet<>(new Comparator<Point>() {
+			@Override
+			public int compare(Main.Point o1, Main.Point o2) {
+				int cmp = Integer.compare(o1.st, o2.st);
+				return cmp;
+			}
+		});
+		TreeSet<Point> set2=new TreeSet<>(new Comparator<Point>() {
+			@Override
+			public int compare(Main.Point o1, Main.Point o2) {
+				int cmp = Integer.compare(o1.ed, o2.ed);
+				return cmp;
+			}
+		});
+		for (int i=0; i<n; i++) {
+			int a = sc.nextInt();
+			int c = sc.nextInt();
+			Point pt=new Point(a, c, i+1);
+			set1.add(pt);
+			set2.add(pt);
+		}
+//		System.out.println(set1);
+//		System.out.println(set2);
+		TreeSet<Integer> set3=new TreeSet<>();
+		while (set1.size()>0) {
+			Point p1=set1.last();
+			set1.remove(p1);
+			set2.remove(p1);
+			set3.add(p1.idx);
+			while (set2.size()>0) {
+				Point p2=set2.last();
+//				System.out.println(p1+" "+p2);
+				if (p2.ed<p1.ed) break;
+				set2.remove(p2);
+				set1.remove(p2);
+			}
+		}
+//		System.out.println(set1);
+//		System.out.println(set2);
+//		System.out.println(set3);
+		System.out.println(set3.size());
+		for (int i3 : set3) {
+			System.out.print(i3+" ");
+		}
+		System.out.println();
+	}
+	static class Point implements Comparable<Point> {
+		final int st;
+		final int ed;
+		final int idx;
+		Point(int st, int ed, int idx) {
+			this.st = st;
+			this.ed = ed;
+			this.idx = idx;
+		}
+		@Override
+		public int compareTo(Main.Point that) {
+			int cmp = Integer.compare(this.st, that.st);
+			if (cmp != 0) return cmp;
+			return Integer.compare(this.ed, that.ed);
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(st, ed);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			Point that = (Point) obj;
+			return this.st == that.st && this.ed == that.ed;
+		}
+		@Override
+		public String toString() {
+			return "(" + st + "," + ed + "," + idx + ")";
+		}
+	}
+}
+/*
+3
+2 4
+1 1
+3 2
+
+5
+1 1
+10 2
+100 3
+1000 4
+10000 5
+
+6
+32 101
+65 78
+2 29
+46 55
+103 130
+52 40
+*/
