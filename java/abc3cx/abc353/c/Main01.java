b1996a8fa5b7b1e9142a596cfe03bb0ab654462e
hiuchida <hiro4uchida@gmail.com>
2025-03-19 09:49:19 +0900

    java/abc353/c

---
+++ java/abc3cx/abc353/c/Main01.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] ary=new int[n];
+		long sum=0;
+		for (int i=0; i<n; i++) {
+			ary[i]=sc.nextInt();
+			sum+=ary[i];
+		}
+		sum*=(n-1);
+		Arrays.sort(ary);
+		System.out.println(Arrays.toString(ary));
+		int i=0;
+		int j=n-1;
+		while (i<n-1) {
+			while (i>=j) j++;
+			while (i<j && ary[i]+ary[j]>=10000*10000) j--;
+//			System.out.println(i+" "+j+" "+(n-1-j));
+			sum-=(n-1-j)*10000L*10000;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
+/*
+3
+3 50000001 50000002
+
+5
+1 3 99999999 99999994 1000000
+*/
