46d5c72548bbab85aaa0ae69d1676427204d58d6
hiuchida <hiro4uchida@gmail.com>
2025-03-21 15:03:52 +0900

    java/abc231/a

---
+++ java/abc2bx/abc231/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		double ans=d/100.0;
+		System.out.println(ans);
+	}
+}
+/*
+1000
+
+50
+
+3141
+*/
