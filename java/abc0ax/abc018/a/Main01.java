88f1db4e844f449701035cdd169c2d9e04c3e6c1
hiuchida <hiro4uchida@gmail.com>
2025-03-21 10:02:24 +0900

    java/abc018/a

---
+++ java/abc0ax/abc018/a/Main01.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Point[] ary=new Point[3];
+		for (int i=0; i<3; i++) {
+			int a = sc.nextInt();
+			ary[i]=new Point(a, i+1);
+		}
+		Arrays.sort(ary);
+		for (int i=2; i>=0; i--) {
+			System.out.println(ary[i].ed);
+		}
+	}
+	static class Point implements Comparable<Point> {
+		final int st;
+		final int ed;
+		Point(int st, int ed) {
+			this.st = st;
+			this.ed = ed;
+		}
+		static Comparator<Point> newComparator1() {
+			return new Comparator<Point>() {
+				@Override
+				public int compare(Point o1, Point o2) {
+					int cmp = Integer.compare(o1.st, o2.st);
+					return cmp;
+				}
+			};
+		}
+		static Comparator<Point> newComparator2() {
+			return new Comparator<Point>() {
+				@Override
+				public int compare(Point o1, Point o2) {
+					int cmp = Integer.compare(o1.ed, o2.ed);
+					return cmp;
+				}
+			};
+		}
+		@Override
+		public int compareTo(Point that) {
+			int cmp = Integer.compare(this.st, that.st);
+			if (cmp != 0) return cmp;
+			return Integer.compare(this.ed, that.ed);
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(st, ed);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			Point that = (Point) obj;
+			return this.st == that.st && this.ed == that.ed;
+		}
+		@Override
+		public String toString() {
+			return "(" + st + "," + ed + ")";
+		}
+	}
+}
+/*
+12
+18
+11
+
+10
+20
+30
+*/
