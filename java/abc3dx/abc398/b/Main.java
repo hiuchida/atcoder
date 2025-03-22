a2fd4958a6e9e6404f73be43e60128c737e1568d
hiuchida <hiro4uchida@gmail.com>
2025-03-23 00:17:51 +0900

    java/abc398/b

---
+++ java/abc3dx/abc398/b/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] ary=new int[20];
+		for (int i=0; i<7; i++) {
+			int a = sc.nextInt();
+			ary[a]++;
+		}
+		int[] cnt=new int[20];
+		for (int i=0; i<ary.length; i++) {
+			cnt[ary[i]]++;
+		}
+		if (cnt[2]>=1 && (cnt[3]>=1 || cnt[4]>=1 || cnt[5]>=1)) System.out.println("Yes");
+		else if (cnt[3]>=1 && cnt[4]>=1) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
+/*
+1 4 1 4 2 1 3
+
+11 12 13 10 13 12 11
+
+7 7 7 7 7 7 7
+
+13 13 1 1 7 4 13
+*/
---
+++ java/abc3dx/abc398/b/README.md
@@ -0,0 +1,29 @@
+# Main01
+int[] aryにaのカウントを取る。
+int[] cntにary\[\]のカウントを取る。
+cnt[2]==1 && cnt[3]==1のときok。
+それ以外はng。
+WA6個。
+
+# Main02
+int[] aryにaのカウントを取る。
+int[] cntにary\[\]のカウントを取る。
+cnt[2]>=1 && cnt[3]>=1のときok。
+それ以外はng。
+WA5個。
+
+# Main03
+int[] aryにaのカウントを取る。
+int[] cntにary\[\]のカウントを取る。
+cnt[2]>=1 && (cnt[3]>=1 || cnt[4]>=1 || cnt[5]>=1)のときok。
+cnt[3]>=1 || cnt[4]>=1のときok。
+それ以外はng。
+WA1個。
+
+# Main
+int[] aryにaのカウントを取る。
+int[] cntにary\[\]のカウントを取る。
+cnt[2]>=1 && (cnt[3]>=1 || cnt[4]>=1 || cnt[5]>=1)のときok。
+cnt[3]>=1 && cnt[4]>=1のときok。
+それ以外はng。
+
