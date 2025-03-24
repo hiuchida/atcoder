11013de4412b22cf88c464fb0c6a38e6a3c4cfe3
hiuchida <hiro4uchida@gmail.com>
2025-03-24 22:13:51 +0900

    java/memo/template

--- java/memo/template/Main.java
+++ java/memo/template/Main.java
@@ -3,12 +3,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int[] ary=new int[n];
+		for (int i=0; i<n; i++) {
+			ary[i]=sc.nextInt();
+		}
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		int d = sc.nextInt();
 		String s = sc.next();
 		StringBuilder sb=new StringBuilder();
+		Arrays.sort(ary);
+		System.out.println(Arrays.toString(ary));
 		System.out.println();
 		System.out.println("Yes");
 		System.out.println("No");
