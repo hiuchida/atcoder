3abb4f183b9b1093a4995466919f580a1e2167cf
hiuchida <hiro4uchida@gmail.com>
2025-03-25 01:38:26 +0900

    java/abc038/a

---
+++ java/abc0bx/abc038/a/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if (s.endsWith("T")) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
+/*
+ICEDT
+
+MUGICHA
+
+OOLONGT
+
+T
+
+TEA
+*/
