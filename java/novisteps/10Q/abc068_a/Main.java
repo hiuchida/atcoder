ecc4dbbc8151d8c20af905cbe6d26bf9a193dbd3
hiuchida <hiro4uchida@gmail.com>
2025-03-29 13:18:48 +0900

    move to novisteps/10Q

--- java/novisteps/10Q/abc068_a/Main.java
+++ java/novisteps/10Q/abc068_a/Main.java
@@ -3,8 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		String s = "ABC" + n;
-		System.out.println(s);
+		String ans="ABC"+n;
+		System.out.println(ans);
 	}
 }
 /*
