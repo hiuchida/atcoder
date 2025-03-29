475dc5f0bd213134d4ebdeaf223c5a3bc669c105
hiuchida <hiro4uchida@gmail.com>
2025-03-29 13:16:49 +0900

    java/abc134/a abc169/a abc180/a

---
+++ java/abc1bx/abc134/a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int ans=3*r*r;
+		System.out.println(ans);
+	}
+}
+/*
+4
+
+15
+
+80
+*/
---
+++ java/abc1dx/abc180/a/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int ans=n-a+b;
+		System.out.println(ans);
+	}
+}
+/*
+100 1 2
+
+100 2 1
+
+100 1 1
+*/
