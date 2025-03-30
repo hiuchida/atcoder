59319ee7dd6f44bd2bd229146439794289493675
hiuchida <hiro4uchida@gmail.com>
2025-03-30 11:54:36 +0900

    java/common/Counter_set Prime

---
+++ java/common/Counter_set.java
@@ -0,0 +1,26 @@
+	static class Counter {
+		Map<Integer, TreeSet<Integer>> map = new TreeMap<>();
+		int size() {
+			return map.size();
+		}
+		TreeSet<Integer> get(int k) {
+			TreeSet<Integer> v = map.get(k);
+			if (v == null) v = new TreeSet<>();
+			return v;
+		}
+		void put(int k, TreeSet<Integer> v) {
+			map.put(k, v);
+		}
+		void add(int k, int idx) {
+			TreeSet<Integer> v = get(k);
+			v.add(idx);
+			put(k, v);
+		}
+		Set<Integer> keySet() {
+			return map.keySet();
+		}
+		@Override
+		public String toString() {
+			return map.toString();
+		}
+	}
