8786b81b060bf3598c5b18815ca7b46ce834de20
hiuchida <hiro4uchida@gmail.com>
2025-04-09 23:16:59 +0900

    move to novisteps/07Q

---
+++ java/novisteps/07Q/abc260_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char[] ary=s.toCharArray();
+		Arrays.sort(ary);
+		if (ary[0]!=ary[1] && ary[1]==ary[2]) System.out.println(ary[0]);
+		else if (ary[0]==ary[1] && ary[1]!=ary[2]) System.out.println(ary[2]);
+		else if (ary[0]==ary[1] && ary[1]==ary[2]) System.out.println(-1);
+		else System.out.println(ary[0]);
+	}
+}
+/*
+pop
+
+abc
+
+xxx
+*/
