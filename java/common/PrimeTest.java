2a907ff35931607f61542b7c2d5d9df2aff3fb55
hiuchida <hiro4uchida@gmail.com>
2025-04-18 03:28:52 +0900

    Prime20250419 fix x>(long)n*n of check

--- java/common/PrimeTest.java
+++ java/common/PrimeTest.java
@@ -1,3 +1,27 @@
+import java.util.*;
+public class Main {
+	//400,000,000 (N=20,000) 2ms 23,729ms 416,974ms
+	//100,000,000 (N=10,000) 1ms  5,196ms 110,924ms
+	//  1,000,000 (N= 1,000) 0ms     22ms     156ms
+	//     10,000 (N=   100) 0ms      1ms       3ms
+	public static void main(String[] args) {
+		final int N=100;
+		long st1=System.currentTimeMillis();
+		Prime pr1=new Prime(N+1);
+		long st2=System.currentTimeMillis();
+		Prime pr2=new Prime(N*N+1);
+		long st3=System.currentTimeMillis();
+		for (int i=0; i<=N*N; i++) {
+			if (pr1.check(i)!=pr2.isp[i]) {
+				System.out.println(i);
+			}
+		}
+		long st4=System.currentTimeMillis();
+		long tm2=st2-st1;
+		long tm3=st3-st2;
+		long tm4=st4-st3;
+		System.out.println("end of check "+tm2+" "+tm3+" "+tm4);
+	}
 	//1,000,000,000 16678ms
 	//  100,000,000  1537ms
 	//   10,000,000    98ms
@@ -53,3 +77,6 @@
 			return ans;
 		}
 	}
+}
+/*
+*/
