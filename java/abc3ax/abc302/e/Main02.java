ea380e5048396c6d5ad1f34c223870011e966394
hiuchida <hiro4uchida@gmail.com>
2025-05-02 02:32:57 +0900

    abc302/e

---
+++ java/abc3ax/abc302/e/Main02.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		Counter cnt=new Counter();
+		for (int qq=0; qq<q; qq++) {
+			int c=sc.nextInt();
+			if (c==1) {
+				int u=sc.nextInt();
+				int v=sc.nextInt();
+				cnt.add(u, v);
+				cnt.add(v, u);
+//				System.out.println(cnt);
+			} else {
+				int v=sc.nextInt();
+				for (int u : cnt.get(v)) {
+					cnt.del(u, v);
+					if (cnt.get(u).size()==0) cnt.remove(u);
+				}
+				cnt.remove(v);
+//				System.out.println(cnt);
+			}
+//			int ans=n-calc(cnt);
+			int ans=n-cnt.size();
+			System.out.println(ans);
+		}
+	}
+//	static int calc(Counter cnt) {
+//		int ans=0;
+//		for (int u : cnt.keySet()) {
+//			if (cnt.get(u).size()>0) ans++;
+//		}
+//		return ans;
+//	}
+	static class Counter { //Counter_int_setint250414
+		Map<Integer, TreeSet<Integer>> map = new TreeMap<>();
+		int size() {
+			return map.size();
+		}
+		TreeSet<Integer> get(int k) {
+			TreeSet<Integer> v = map.get(k);
+			if (v == null) v = new TreeSet<>();
+			return v;
+		}
+		void put(int k, TreeSet<Integer> v) {
+			map.put(k, v);
+		}
+		void remove(int k) {
+			map.remove(k);
+		}
+		boolean is(int k, int idx) {
+			TreeSet<Integer> v = get(k);
+			return v.contains(idx);
+		}
+		void add(int k, int idx) {
+			TreeSet<Integer> v = get(k);
+			v.add(idx);
+			put(k, v);
+		}
+		void del(int k, int idx) {
+			TreeSet<Integer> v = get(k);
+			v.remove(idx);
+			put(k, v);
+		}
+		NavigableSet<Integer> keySet() {
+			return (NavigableSet<Integer>) map.keySet();
+		}
+		@Override
+		public String toString() {
+			return map.toString();
+		}
+	}
+}
+/*
+3 7
+1 1 2
+1 1 3
+1 2 3
+2 1
+1 1 2
+2 2
+1 1 2
+
+2 1
+2 1
+*/
---
+++ java/abc3ax/abc302/e/Main10.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		Counter cnt=new Counter();
+		int ans=n;
+		for (int qq=0; qq<q; qq++) {
+			int c=sc.nextInt();
+			if (c==1) {
+				Integer u=Integer.valueOf(sc.nextInt());
+				Integer v=Integer.valueOf(sc.nextInt());
+				if (cnt.size(u)==0) ans--;
+				if (cnt.size(v)==0) ans--;
+				cnt.add(u, v);
+				cnt.add(v, u);
+//				System.out.println(cnt);
+			} else {
+				Integer v=Integer.valueOf(sc.nextInt());
+				for (Integer u : cnt.get(v)) {
+					cnt.del(u, v);
+					if (cnt.size(u)==0) ans++;
+				}
+				if (cnt.size(v)>0) ans++;
+				cnt.remove(v);
+//				System.out.println(cnt);
+			}
+			System.out.println(ans);
+		}
+	}
+	static class Counter { //Counter_int_setint250502
+		Map<Integer, HashSet<Integer>> map = new HashMap<>();
+		int size() {
+			return map.size();
+		}
+		int size(Integer k) {
+			HashSet<Integer> v = map.get(k);
+			if (v == null) return 0;
+			return v.size();
+		}
+		HashSet<Integer> get(Integer k) {
+			HashSet<Integer> v = map.get(k);
+			if (v == null) v = new HashSet<>();
+			return v;
+		}
+		void put(int k, HashSet<Integer> v) {
+			map.put(k, v);
+		}
+		void remove(Integer k) {
+			map.remove(k);
+		}
+		boolean is(Integer k, Integer idx) {
+			HashSet<Integer> v = get(k);
+			return v.contains(idx);
+		}
+		void add(Integer k, Integer idx) {
+			HashSet<Integer> v = get(k);
+			v.add(idx);
+			put(k, v);
+		}
+		void del(Integer k, Integer idx) {
+			HashSet<Integer> v = get(k);
+			v.remove(idx);
+			put(k, v);
+		}
+		NavigableSet<Integer> keySet() {
+			return (NavigableSet<Integer>) map.keySet();
+		}
+		@Override
+		public String toString() {
+			return map.toString();
+		}
+	}
+}
+/*
+3 7
+1 1 2
+1 1 3
+1 2 3
+2 1
+1 1 2
+2 2
+1 1 2
+
+2 1
+2 1
+*/
